c07fff710c18cc3a940281b7a653b5ad793cf232
Xiangmin Ru <xiru@microsoft.com>
2024-11-05 16:50:05 +0800

    Support PPT/Excel

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -14,7 +14,7 @@ async function insertParagraph(event) {
   try {
     await Word.run(async (context) => {
       const body = context.document.body;
-      body.insertParagraph("Hello World", Word.InsertLocation.end);
+      body.insertParagraph("Hello World, this is the line added automatically when document is opened.", Word.InsertLocation.end);
       await context.sync();
     });
   } catch (error) {
@@ -24,6 +24,56 @@ async function insertParagraph(event) {
 
   // Calling event.completed is required. event.completed lets the platform know that processing has completed.
   event.completed();
+}
+
+async function insertPPT(event) {
+  try{
+    await PowerPoint.run(async (context) => {
+      const shapes: PowerPoint.ShapeCollection = context.presentation.slides.getItemAt(0).shapes;
+      const shapeOptions: PowerPoint.ShapeAddOptions = {
+        left: 100,
+        top: 300,
+        height: 300,
+        width: 450
+      };
+      const textbox: PowerPoint.Shape = shapes.addTextBox("Hello, World! This is the first Shape automatically added when opening.", shapeOptions);
+  
+      await context.sync();
+    });
+  } catch (error) {
+    // Note: In a production add-in, notify the user through your add-in's UI.
+    //console.error(error);
+  }
+
+  // Calling event.completed is required. event.completed lets the platform know that processing has completed.
+  event.completed();
+}
+
+async function insertExcelTable() {
+  await Excel.run(async (context) => {
+    context.workbook.worksheets.getItemOrNullObject("Sample").delete();
+    const sheet = context.workbook.worksheets.add("Sample");
+
+    const data = [
+      ["Table data", "Added", "Through", "Autorun"],
+      ["Product", "Qty", "Unit Price", "Total Price"],
+      ["Almonds", 2, 7.5, "=C3 * D3"],
+      ["Coffee", 1, 34.5, "=C4 * D4"],
+      ["Chocolate", 5, 9.56, "=C5 * D5"]
+    ];
+
+    const range = sheet.getRange("B1:E5");
+    range.values = data;
+    range.format.autofitColumns();
+
+    const header = range.getRow(1);
+    header.format.fill.color = "#4472C4";
+    header.format.font.color = "white";
+
+    sheet.activate();
+
+    await context.sync();
+  });
 }
 
 function getGlobal() {
@@ -39,3 +89,5 @@ function getGlobal() {
 const g = getGlobal() as any;
 
 Office.actions.associate("insertParagraph", insertParagraph);
+Office.actions.associate("insertPPT", insertPPT);
+Office.actions.associate("insertExcelTable", insertExcelTable);
